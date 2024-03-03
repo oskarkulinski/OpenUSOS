793be33415d2e74756fc5ba07e9b66abfc8f3ccb
filiera <filip.ciebiera@gmail.com>
2024-03-03 19:09:33 +0100

    uososapi + test
    wysyłam usosapi.py, dzieki któremu korzystamy z api usosowego. pozatym wysyłam projekt_test.py, gdzie jest przetestowana autoryzacja i wysyłanie widomości.

---
+++ projekt_test.py
@@ -0,0 +1,81 @@
+import usosapi
+
+# USOS API Base URL, trailing slash included.
+Usosapi_base_url = 'https://apps.usos.uj.edu.pl/'
+
+# Consumer Key to use.
+Consumer_key = '748apdbMm3Ggh6KGXMyp'
+Consumer_secret = 'Y33JYY3vgjNym6aFP5qnxcMtE7WrejLt8VDjWS87'
+
+# We use usosapi.py in order to do EVERYTHING much easier:
+
+# We create connection object (We will be using that to call requests): 
+# Valid consumer_key and consumer_secret is needed for that:
+Main = usosapi.USOSAPIConnection(Usosapi_base_url, Consumer_key, Consumer_secret)
+
+def Send_an_email():
+    Subject = input('Input the subject for your message: ')
+    Content = input('Input the content of your message: ')
+    Recepient = input('Input an e-mail of the recepient: ')
+
+    Recepient_list = {Recepient}
+    # We create the message:
+    Message_id = Main.get('services/mailclient/create_message', subject=Subject, content=Content)
+
+    # We update (create) recepient:
+    Main.get('services/mailclient/update_recipients_group', message_id=Message_id["message_id"], emails=Recepient_list)
+    # Then we refresh them:
+    Main.get('services/mailclient/refresh_recipients', message_id=Message_id["message_id"] )
+
+    # Finally, we send the message:
+
+    Main.get('services/mailclient/send_message', message_id=Message_id["message_id"])
+
+
+
+
+# Some function to choose what user wants
+def Choose_What_To_Do():
+    print("Welcome to SUS!")
+    print("")
+    print("What would you like to do?")
+
+    print("1: Register for a course (not yet possible)" )
+    print("2: Make a mail (quite possible)")
+    Choice = int(input('Please, input a number '))
+    while Choice not in {1, 2}:
+        Choice = int(input('Please, input a VALID number '))
+    if Choice == 1:
+        print('nah')
+        pass
+    if Choice == 2:
+        Send_an_email()
+
+
+#--------------------------------------------------------------------------------------------
+# 'Main' here
+#--------------------------------------------------------------------------------------------
+
+# Test  to check if connection is valid:
+print(Main.test_connection())
+
+# User has to visit using some internet browser to obtain a PIN code required for authorization.
+# IDK, you do frontend lul
+AuthURL = Main.get_authorization_url()
+print(AuthURL) # Pass it to user somehow
+PIN = input('What is the PIN code? ')
+PIN.replace(" ", "")
+# Authorization:
+Main.authorize_with_pin(PIN)
+# test: return users firstname, lastname, id:
+lalala = Main.current_identity()
+print(lalala)
+# Access token, can be used to continue a session:
+Access = Main.get_access_data()
+print(Access) # We should probably save it, so that user doesn't have to log in all the time.
+
+# Now, do stuff:
+Choose_What_To_Do()
+
+# Logging out:
+Main.logout()
